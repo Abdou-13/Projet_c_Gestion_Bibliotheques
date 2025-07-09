6adff88d3d5d9230a83763ea4c071fdcd698dc0d
fatim1910 <fatimpouye04@gmail.com>
2025-07-09 17:23:10 +0200

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -1,1 +1,33 @@
+int supprimerLivre() {
+    system("cls");
+    system("color 5");
+    int isbn;
+    printf("Entrez le code ISBN du livre à supprimer: ");
+    scanf("%d", &isbn);
+    getchar();
 
+    int index = -1;
+
+    for (int i = 0; i < bibliotheque.nombreLivres; i++) {
+        if (bibliotheque.livres[i].ISBN == isbn) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1) {
+        printf("Aucun livre trouve.\n");
+        return 0;
+    }
+
+    for (int i = index; i < bibliotheque.nombreLivres - 1; i++) {
+        bibliotheque.livres[i] = bibliotheque.livres[i + 1];
+    }
+
+    bibliotheque.nombreLivres--;
+    printf("Livre supprime.\n");
+    return 1;
+
+    system("pause");
+    system("cls");
+}
