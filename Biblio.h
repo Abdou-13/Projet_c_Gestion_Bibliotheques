80cab4619d896b3981dcb2ee76dab48e02253db6
dev_Abdou <abdoumelo2135@gmail.com>
2025-07-09 15:39:09 +0000

    Biblio.h

--- Biblio.h
+++ Biblio.h
@@ -1,9 +1,3 @@
-void enregistrerLivre();
-void afficherLivres();
-void rechercherLivre();
-int supprimerLivre();
+ 
 int gererEmprunt();
-void afficherStatistiques();
-void afficherMenu();
- float  calcul();
-
+ 
