bb5c33a29231ce3ab0254c497746ecb5a5acf9e1
trevor911 <papeomarn58@gmail.com>
2025-07-09 15:23:04 +0000

    papa.biblio.c

--- biblio.c
+++ biblio.c
@@ -1,1 +1,22 @@
+void afficherStatistiques() {
+    system("cls");
+    system("color 6");
+    int totalLivres = bibliotheque.nombreLivres;
+    int livresEmpruntes = 0;
 
+    for (int i = 0; i < bibliotheque.nombreLivres; i++) {
+        if (bibliotheque.livres[i].estEmprunte) {
+            livresEmpruntes++;
+        }
+    }
+
+    printf("\n=== Statistiques de la Bibliotheque ===\n");
+    printf("Total de livres: %d\n", totalLivres);
+    printf("Livres empruntes: %d\n", livresEmpruntes);
+    printf("Livres disponibles: %d\n", totalLivres - livresEmpruntes);
+    printf("Nombre total d'emprunts enregistres: %d\n", bibliotheque.nombreEmprunts);
+
+    puts("");
+    system("pause");
+    system("cls");
+}
