b3bb4a118616b86bd10a309eff1c843fbab2cb3e
Babacar TOURE <babacartoure862@gmail.com>
2025-07-09 17:20:14 +0200

    BabacarToure.biblio.c

--- biblio.c
+++ biblio.c
@@ -1,1 +1,22 @@
+void afficherLivres() {
+    system("cls");
+    system("color B");
+   int trouves = 0;
+    printf("\n=== Livres disponibles ===\n");
+    for (int i = 0; i < bibliotheque.nombreLivres; i++) {
+        if (!bibliotheque.livres[i].estEmprunte) {
+            printf("%d. %s - %s (%d) [ISBN: %d]\n",
+                   i + 1,
+                   bibliotheque.livres[i].titre,
+                   bibliotheque.livres[i].auteur,
+                   bibliotheque.livres[i].anneePublication,
+                   bibliotheque.livres[i].ISBN);
+            trouves++;
+        }
+    }
+    if (trouves == 0)
+        printf("Aucun livre disponible.\n");
 
+    system("pause");
+    system("cls");
+}
