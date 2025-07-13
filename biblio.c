ae58f60c43d4900548c87af716746518a61c5d25
Babacar TOURE <babacartoure862@gmail.com>
2025-07-13 17:17:48 +0200

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -1,22 +1,25 @@
 void afficherLivres() {
     system("cls");
     system("color B");
-   int trouves = 0;
+   int livresDisponibles= 0;
     printf("\n=== Livres disponibles ===\n");
+    printf(" Titre - Auteur (Année) [ISBN]\n");
     for (int i = 0; i < bibliotheque.nombreLivres; i++) {
         if (!bibliotheque.livres[i].estEmprunte) {
-            printf("%d. %s - %s (%d) [ISBN: %d]\n",
+            printf("%d.\t%s - %s (%d) [ISBN: %d]\n",
                    i + 1,
                    bibliotheque.livres[i].titre,
                    bibliotheque.livres[i].auteur,
                    bibliotheque.livres[i].anneePublication,
                    bibliotheque.livres[i].ISBN);
-            trouves++;
+            livresDisponibles++;
         }
     }
-    if (trouves == 0)
+    if (livresDisponibles == 0){
         printf("Aucun livre disponible.\n");
-
+    }else {
+        printf("\nTotal : %d livres disponible. \n", livresDisponibles);
+    }
     system("pause");
     system("cls");
 }
