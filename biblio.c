160dc8574e2ba82eebdbb5dbd8933cf93b2213e9
Babacar TOURE <babacartoure862@gmail.com>
2025-07-14 17:19:47 +0200

    Update biblio.c

--- biblio.c
+++ biblio.c
@@ -3,10 +3,9 @@ void afficherLivres() {
     system("color B");
    int livresDisponibles= 0;
     printf("\n=== Livres disponibles ===\n");
-    printf(" Titre - Auteur (Année) [ISBN]\n");
     for (int i = 0; i < bibliotheque.nombreLivres; i++) {
         if (!bibliotheque.livres[i].estEmprunte) {
-            printf("%d.\t%s - %s (%d) [ISBN: %d]\n",
+            printf("%d.%s - %s (%d) [ISBN: %d]\n",
                    i + 1,
                    bibliotheque.livres[i].titre,
                    bibliotheque.livres[i].auteur,
